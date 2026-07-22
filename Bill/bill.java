23b55b1dd3304870a95ad8981297d60d8d94ff56
Harkeerat-ai <harkeeratbhasin69@gmail.com>
2026-07-22 18:19:30 +0530

    Bill generator

---
+++ Bill/bill.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class bill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder bill = new StringBuilder();
+        System.out.print("How many items did you buy : ");
+        int count = sc.nextInt();
+        String item[] = new String[count];
+        int item_price[] = new int[count];
+        for(int i=0; i<count; i++){
+            System.out.print("Name of item : ");
+            item[i] = sc.next();
+            System.out.print("What is the cost : ");
+            item_price[i] = sc.nextInt();
+        }
+        int total=0;
+        for(int i=0; i<item_price.length; i++){
+            total += item_price[i];
+        }
+
+        bill.append("******** SHOP BILL ********\n");
+        for(int i=0; i<item.length; i++){
+            bill.append(item[i] + "      : Rs.").append(item_price[i]).append("\n");
+        }
+        bill.append("--------------------------\n");
+        bill.append("Total     : Rs.").append(total);
+
+        System.out.println(bill);
+    }
+}
