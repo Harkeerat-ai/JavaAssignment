82acab85751adb1464ef8d7f06c001e0f5698313
Harkeerat-ai <harkeeratbhasin69@gmail.com>
2026-07-22 16:29:49 +0530

    Tranpose of matrix

---
+++ MatrixTranspose/MatrixTranspose.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class MatrixTranspose{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Please enter rows of the matrix : ");
+        int rows = sc.nextInt();
+        System.out.print("Please enter columns of the matrix : ");
+        int columns = sc.nextInt();
+
+        int arr[][] = new int[rows][columns];
+
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                System.out.print("Please enter element at position : " + i + "," + j + " : ");
+                arr[i][j] = sc.nextInt();
+            }
+        }
+        System.out.println("Matrix inputted : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+        System.out.println("Transpose Matrix : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++){
+                System.out.print(arr[j][i]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
